be4b0333de2a034c5f12465af66296b8c5ae3943
arman786mariyam <mobashirahmad507@gmail.com>
2025-02-03 16:53:37 +0530

    Update m.py

--- m.py
+++ m.py
@@ -12,7 +12,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from threading import Thread
 
 loop = asyncio.get_event_loop()
-TOKEN = '8049040389:AAEbFdKMXzozTErb6M9d5SpABX0OE3l8CII'
+TOKEN = '8049040389:AAEqyFUHDXsfJgTL_pRb48NQK9XwOnBeERY'
 MONGO_URI = 'mongodb+srv://Arman121:Arman121@arman.ji8gqxd.mongodb.net/?retryWrites=true&w=majority'
 CHANNEL_ID = -1002357119943
 
